5213393879a6356eb9d1f57e4cbc5fe3b2df8a59
Eduardo <legna246.dav@gmail.com>
2024-02-20 12:09:49 -0600

    03 App Career Model Career

---
+++ seani/career/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Career(models.Model):
+
+    LEVELS = [
+        ('Ing', 'Ingeniería'),
+        ('TSU', 'Técnico Superior Universitario'),
+        ('M', 'Maestría')
+    ]
+
+    name = models.CharField(verbose_name = 'Nombre', max_length = 200)
+    short_name = models.CharField(verbose_name = 'Abreviatura', max_length = 20)
+    level = models.CharField(verbose_name = 'Nivel', max_length = 10, choices = LEVELS)
+
+    def __str__(self):
+        return f"{ self.level } - { self.short_name }"
+
+    class Meta:
+        verbose_name = 'carrera'
+        verbose_name_plural = 'carreras'
