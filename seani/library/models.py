4139668c2c3e5a0d7ffe20a98d81ceae6f9d5d9d
Eduardo <legna246.dav@gmail.com>
2024-02-22 13:11:48 -0600

    04 implements Cloudinary customize Models

--- seani/library/models.py
+++ seani/library/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+from cloudinary.models import CloudinaryField
+
 # Create your models here.
 class Module(models.Model):
     name = models.CharField(verbose_name= "Nombre", max_length = 100)
     description = models.CharField(verbose_name= "Descripción", max_length = 200)
+
+    def num_questions(self):
+        return self.question_set.count()
+    num_questions.short_description = 'Número de preguntas'
 
     def __str__(self):
         return self.name
@@ -16,7 +22,14 @@ class Module(models.Model):
 class Question(models.Model):
     module = models.ForeignKey(Module, on_delete = models.CASCADE, verbose_name = 'Módulo')
     question_text = models.CharField(verbose_name = 'Texto de la pregunta', max_length = 200, null = True, blank = True)
-    question_image = models.ImageField(verbose_name='Imagen de la pregunta', upload_to='questions', null = True, blank = True)
+    # question_image = models.ImageField(verbose_name='Imagen de la pregunta', upload_to='questions', null = True, blank = True)
+    question_image = CloudinaryField(
+        verbose_name = 'Imagen de la pregunta',
+        folder = 'question',
+        resource_type = 'image',
+        null = True,
+        blank = True
+    )
     answer1 = models.CharField(verbose_name='Respuesta A', max_length = 200)
     answer2 = models.CharField(verbose_name='Respuesta B', max_length = 200)
     answer3 = models.CharField(verbose_name='Respuesta C', max_length = 200, null = True, blank =  True)
