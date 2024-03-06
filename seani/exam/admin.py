348cbb97267d951f95cde0d68eb4890a34db8275
Eduardo <legna246.dav@gmail.com>
2024-03-06 09:35:50 -0600

    05 implements Exam, Breakdown and ExamModule modules

--- seani/exam/admin.py
+++ seani/exam/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from .models import Stage, Exam, ExamModule
 
 from .models import Stage
 
 @admin.register(Stage)
 class StageAdmin(admin.ModelAdmin):
     list_display = ['stage', 'month', 'year']
+
+class ExamModuleInline(admin.TabularInline):
+    model = ExamModule
+    extra = 1
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ['user', 'score', 'career']
+    inlines = [ExamModuleInline]
